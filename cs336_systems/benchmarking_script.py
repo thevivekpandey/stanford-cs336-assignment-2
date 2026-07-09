8ebe5c282b15f63c1167b13a90b65d0890aed143
Vivek Pandey <thevivekpandey@gmail.com>
2026-07-09 05:54:55 +0000

    some changes to benchmarking script

--- cs336_systems/benchmarking_script.py
+++ cs336_systems/benchmarking_script.py
@@ -1,11 +1,13 @@
 import sys
 import os
 import torch
+import timeit
 # Add the parent directory to Python path
 sys.path.insert(0, os.path.abspath('../cs336-basics'))
 
 from cs336_basics.model import BasicsTransformerLM
 from cs336_basics.optimizer import AdamW
+from cs336_basics.nn_utils import cross_entropy
 
 VOCAB_SIZE = 10000
 BATCH_SIZE = 4
@@ -49,14 +51,13 @@ CONFIG = {
     }
 }
 
-def get_random_data():
-    a = torch.randint(0, VOCAB_SIZE, size=(BATCH_SIZE, CONTEXT_LENGTH))
-    b = torch.randint(0, VOCAB_SIZE, size=(BATCH_SIZE, CONTEXT_LENGTH))
+def get_random_data(device: str):
+    a = torch.randint(0, VOCAB_SIZE, size=(BATCH_SIZE, CONTEXT_LENGTH)).to(device)
+    b = torch.randint(0, VOCAB_SIZE, size=(BATCH_SIZE, CONTEXT_LENGTH)).to(device)
     return a, b
 
-def benchmarking_script():
-    model_type = "small"
-    print("Initing the model")
+def benchmarking_script(model_type):
+    DEVICE = "cuda"
     model = BasicsTransformerLM(
         VOCAB_SIZE,
         CONTEXT_LENGTH,
@@ -66,13 +67,53 @@ def benchmarking_script():
         CONFIG[model_type]["d_ff"],
         10_000
     )
-    print("Initing the optimizer")
+    model.to(DEVICE)
+
     optimizer = AdamW(model.parameters())
-    print("Getting data")
-    i, t = get_random_data()
-    print("Running forward pass")
-    logits = model(i)
-    print(logits)
+    model.train()
+    for i in range(20):
+        t1 = timeit.default_timer()
+
+        a, b = get_random_data(DEVICE)
+        torch.cuda.synchronize()
+
+        t2 = timeit.default_timer()
+
+        optimizer.zero_grad()
+        torch.cuda.synchronize()
+
+        t3 = timeit.default_timer()
+
+        logits = model(a)
+        torch.cuda.synchronize()
+
+        t4 = timeit.default_timer()
+
+        loss = cross_entropy(logits, b)
+        torch.cuda.synchronize()
+
+        t5 = timeit.default_timer()
+
+        loss.backward()
+        torch.cuda.synchronize()
+
+        t6 = timeit.default_timer()
+        optimizer.step()
+
+        torch.cuda.synchronize()
+        print(model_type, 
+              i, 
+              round(t2 - t1, 4), 
+              round(t3 - t2, 4), 
+              round(t4 - t3, 4), 
+              round(t5 - t4, 4), 
+              round(t6 - t5, 4))
+
 
 if __name__ == "__main__":
-    benchmarking_script()
+    benchmarking_script("xl")
+    benchmarking_script("small")
+    benchmarking_script("medium")
+    benchmarking_script("large")
+    benchmarking_script("xl")
+    benchmarking_script("10B")
